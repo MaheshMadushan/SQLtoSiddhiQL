4f18d6c5e9eb3281b2c3dfed8993da60e6e6df5a
Mahesh <58337761+MaheshMadushan@users.noreply.github.com>
2022-11-21 23:12:34 +0530

    Initiating Design for filtering

--- src/main/java/SiddhiApp/SiddhiApp.java
+++ src/main/java/SiddhiApp/SiddhiApp.java
@@ -1,15 +1,21 @@
 package SiddhiApp;
 
+import SiddhiApp.Statement.FilterExpressionStatement.FilterExpression;
+import SiddhiApp.Statement.FilterExpressionStatement.IFilterExpression;
 import SiddhiApp.Statement.Select.SelectStatement;
 
 public class SiddhiApp {
     // create define stream
     DefineStreamStatement defineStreamStatement = new DefineStreamStatement("InputStream");
-    // from statement
     // select statement
     SelectStatement selectStatement = new SelectStatement();
+    // from statement
+    // filter statement
+    IFilterExpression filterExpression = new FilterExpression();
 
-
+    public void addSymbolToFilterExpression(String symbol){
+        ((FilterExpression) this.filterExpression).addSymbol(symbol);
+    }
 
     public void addSelectItem(ISiddhiAppComposite selectItem){
         selectStatement.addSelectItem(selectItem);
@@ -18,6 +24,7 @@ public class SiddhiApp {
     public void addColumnWithDataType(ISiddhiAppComposite columnWithDataType){
         defineStreamStatement.addAttributeWithDataType(columnWithDataType);
     }
+
     public String getSelectItemListAsString(){
         System.out.println(selectStatement.getSiddhiAppCompositeAsString());
         System.out.println(defineStreamStatement.getSiddhiAppCompositeAsString());
