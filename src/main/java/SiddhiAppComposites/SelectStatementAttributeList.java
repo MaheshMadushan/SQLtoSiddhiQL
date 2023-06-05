e6024dbb5d2cc0235b72f912d485f40b52b99794
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-06-05 14:21:35 +0530

    Change the 'select ...' string in siddhi app as it supports to join queries

--- src/main/java/SiddhiAppComposites/SelectStatementAttributeList.java
+++ src/main/java/SiddhiAppComposites/SelectStatementAttributeList.java
@@ -1,10 +1,10 @@
 package SiddhiAppComposites;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
 public class SelectStatementAttributeList implements IAttributeList{
 
     private final List<ISiddhiAppComposite> attributesWithOrWithoutAliasesList;
+    private final Map<String, List<ISiddhiAppComposite>> attributesListWithStreamName = new HashMap<>();
 
     public SelectStatementAttributeList() {
         this.attributesWithOrWithoutAliasesList = new ArrayList<>(10);
@@ -17,6 +17,99 @@ public class SelectStatementAttributeList implements IAttributeList{
     public void addAttribute(ISiddhiAppComposite attribute){
         attributesWithOrWithoutAliasesList.add(attribute);
     }
+
+    public void addAttribute(String streamName, ISiddhiAppComposite attribute) {
+        if (attributesListWithStreamName.get(streamName) != null) {
+            attributesListWithStreamName.get(streamName).add(attribute);
+        } else {
+            List<ISiddhiAppComposite> siddhiAppComposites = new ArrayList<>();
+            siddhiAppComposites.add(attribute);
+            attributesListWithStreamName.put(streamName, siddhiAppComposites);
+        }
+    }
+
+    public String generateString() {
+        StringBuilder attributeSetWithAliasesWithOutDataType = new StringBuilder();
+
+
+        for (ISiddhiAppComposite iSiddhiAppComposite: attributesWithOrWithoutAliasesList) {
+            Iterator<Map.Entry<String, List<ISiddhiAppComposite>>> attributesIterator = attributesListWithStreamName.entrySet().iterator();
+            boolean hasNextEntry = attributesIterator.hasNext();
+            while (hasNextEntry) {
+                Map.Entry<String, List<ISiddhiAppComposite>> entry = attributesIterator.next();
+                String streamName = entry.getKey();
+                List<ISiddhiAppComposite> attributeList = attributesListWithStreamName.get(streamName);
+                if (attributeList.contains(iSiddhiAppComposite)) {
+                    attributeSetWithAliasesWithOutDataType
+                            .append(streamName)
+                            .append(".")
+                            .append(iSiddhiAppComposite.getSiddhiAppCompositeAsString().replaceAll("\\s", ""))
+                            .append(", ");
+                    break;
+                }
+
+            }
+        }
+
+        if (attributeSetWithAliasesWithOutDataType.length() > 1) {
+            attributeSetWithAliasesWithOutDataType.setLength(attributeSetWithAliasesWithOutDataType.length() - 2);
+        }
+
+        return attributeSetWithAliasesWithOutDataType.toString();
+    }
+//    public String generateString() {
+//        StringBuilder attributeSetWithAliasesWithOutDataType = new StringBuilder();
+//
+//        Iterator<Map.Entry<String, List<ISiddhiAppComposite>>> attributesIterator = attributesListWithStreamName.entrySet().iterator();
+//
+//        boolean hasNextEntry = attributesIterator.hasNext();
+//
+//        while (hasNextEntry) {
+//            Map.Entry<String, List<ISiddhiAppComposite>> entry = attributesIterator.next();
+//            String streamName = entry.getKey();
+//            List<ISiddhiAppComposite> attributesWithOrWithoutAliasesList = entry.getValue();
+//
+//            Iterator<ISiddhiAppComposite> attributesWithOrWithoutAliasesIterator = attributesWithOrWithoutAliasesList.iterator();
+//            boolean hasNextComponent = attributesWithOrWithoutAliasesIterator.hasNext();
+//
+//            while (hasNextComponent) {
+//                ISiddhiAppComposite selectItemComposite = attributesWithOrWithoutAliasesIterator.next();
+//                hasNextComponent = attributesWithOrWithoutAliasesIterator.hasNext();
+//
+//                String attributeString = selectItemComposite.getSiddhiAppCompositeAsString().replaceAll("\\s", "");
+//
+//                if (attributesListWithStreamName.containsKey(attributeString)) {
+//                    List<ISiddhiAppComposite> streamAttributes = attributesListWithStreamName.get(attributeString);
+//                    for (ISiddhiAppComposite streamAttribute : streamAttributes) {
+//                        attributeSetWithAliasesWithOutDataType
+//                                .append(streamName)
+//                                .append(".")
+//                                .append(streamAttribute.getSiddhiAppCompositeAsString().replaceAll("\\s", ""))
+//                                .append(",");
+//                    }
+//                } else {
+//                    attributeSetWithAliasesWithOutDataType
+//                            .append(streamName)
+//                            .append(".")
+//                            .append(attributeString)
+//                            .append(",");
+//                }
+//            }
+//
+//            hasNextEntry = attributesIterator.hasNext();
+//            if (hasNextEntry) {
+//                attributeSetWithAliasesWithOutDataType.append(",");
+//            }
+//        }
+//
+//// Remove the trailing comma if the string is not empty
+//        if (attributeSetWithAliasesWithOutDataType.length() > 0) {
+//            attributeSetWithAliasesWithOutDataType.setLength(attributeSetWithAliasesWithOutDataType.length() - 1);
+//        }
+//
+//        return attributeSetWithAliasesWithOutDataType.toString();
+//
+//    }
 
     @Override
     public String toString() {
@@ -52,5 +145,9 @@ public class SelectStatementAttributeList implements IAttributeList{
     @Override
     public String getSiddhiAppCompositeAsString() {
         return toString();
+    }
+
+    public String getSiddhiAppCompositeAsStringInJoins() {
+        return generateString();
     }
 }
